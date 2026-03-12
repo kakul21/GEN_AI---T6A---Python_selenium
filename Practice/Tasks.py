afb60cf36f2c4c8458622c600af37ec479c96a89
kakul21 <132839058+kakul21@users.noreply.github.com>
2026-03-12 17:57:12 +0530

    Task done

--- Practice/Tasks.py
+++ Practice/Tasks.py
@@ -4,7 +4,18 @@ o = ChromeOptions()
 o.add_experimental_option("detach", True)
 driver = Chrome(options=o)
 
-driver.get("https://www.wikipedia.com")
+# Task 1
+'''driver.get('https://www.amazon.com')
+driver.maximize_window()
+print(driver.title)'''
+
+# Task 2
+'''driver.get('https://www.amazon.com')
+driver.maximize_window()
+print(driver.current_url)'''
+
+# Task 3
+'''driver.get("https://www.wikipedia.com")
 driver.maximize_window()
 driver.refresh()
 print(driver.title)
@@ -14,7 +25,4 @@ sleep(5)
 print(driver.title)
 print(driver.current_url)
 sleep(5)
-driver.back()
-sleep(5)
-driver.forward()
-driver.close()
+driver.close()'''
