43a96bd0f14cfbd41d769b507ecc62e57798363f
kakul21 <132839058+kakul21@users.noreply.github.com>
2026-04-01 22:40:31 +0530

    Add files via upload

---
+++ Practice/TaskAPI.py
@@ -0,0 +1,13 @@
+import requests
+
+## Task 1
+## Implement Assert
+'''response = requests.get("https://petstore.swagger.io/v2/pet/1")
+actual = response.status_code
+expected = 200
+assert actual == expected, actual
+print("Correct")'''
+
+## TASK 2: Pytest Markers Practice - in Test_markers.py
+
+
